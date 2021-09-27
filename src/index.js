b83187dce40040c9ade0d6e833db0756481ba5b1
alexander16108 <alexandermayowa05@gmail.com>
2021-09-27 11:31:43 +0100

    fixing linter errors

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function displayTask() {
   if (content !== []) {
     content.forEach((element) => {
       const structure = ` <li class='List-items' id='${element.index}'>
-      <input type='checkbox' class='box' data-id='${element.index}'  ${element.completed ? 'checked' : ''}>
+      <input type='checkbox' class='box' data-id='${element.index}'  ${element.completed ?  'checked' : ''}>
       <input type='text' value='${element.description}' data-index='${element.index}' class='description ${element.completed ?'completed' : ''}'>
       <i class='fas fa-ellipsis-v move-element' data-id='${element.index}'></i>
       </li>
