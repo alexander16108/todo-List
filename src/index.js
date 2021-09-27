c083465a05d8e8aeaf60423e21eff596197e74e0
alexander16108 <alexandermayowa05@gmail.com>
2021-09-27 13:08:55 +0100

    fixing linter error in javascript

--- src/index.js
+++ src/index.js
@@ -30,13 +30,13 @@ const content = JSON.parse(localStorage.getItem('taskContent')) || [{
 ];
 
 function displayTask() {
-  if (content != []) {
+  if (content !== []) {
     content.forEach((element) => {
       const structure = ` <li class='List-items' id='${element.index}'>
       <input type='checkbox' class='box' data-id='${element.index}'  ${element.completed ? 'checked' : ''}>
       <input type='text' value='${element.description}' data-index='${element.index}' class='description ${element.completed ? 'completed' : ''}'>
       <i class='fas fa-ellipsis-v move-element' data-id='${element.index}'></i>
-      </li> `
+      </li> `;
       container.innerHTML += structure;
     });
   }
