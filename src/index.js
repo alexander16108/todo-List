ac39f8f93d83332ecd68e86396d054bc65037cbd
alexander16108 <alexandermayowa05@gmail.com>
2021-09-27 11:06:27 +0100

    fixing linter errors

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import './style.css';
+
 const container = document.getElementById('output-container');
 const content = [
   {
     index: 0,
     completed: true,
-    description: 'wash the dishes'
+    description: 'wash the dishes',
   },
   {
     index: 1,
     completed: false,
-    description: 'wash the dishes'
+    description: 'wash the dishes',
   },
   {
     index: 2,
     completed: true,
-    description: 'wash the dishes'
+    description: 'wash the dishes',
   },
   {
     index: 3,
     completed: false,
-    description: 'wash the dishes'
+    description: 'wash the dishes',
   },
 ];
 
 function displayTask() {
-  if (content != []) {
+  if (content !== []) {
     content.forEach((element) => {
      const structure = ` <li class='List-items' id='${element.index}'>
                    <input type='checkbox' class='box' data-id='${element.index
