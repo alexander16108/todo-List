88fe19d34d1cb4dc895174a73a71eeeeda1d6a98
alexander16108 <alexandermayowa05@gmail.com>
2021-09-27 13:03:01 +0100

    Updatinjg html file to host upcoming changes

--- src/index.js
+++ src/index.js
@@ -1,42 +1,54 @@
 import './style.css';
+import updateStatus from './check'
 
 const container = document.getElementById('output-container');
-const content = [
-  {
-    index: 0,
-    completed: true,
-    description: 'wash the dishes',
-  },
-  {
-    index: 1,
-    completed: false,
-    description: 'wash the dishes',
-  },
-  {
-    index: 2,
-    completed: true,
-    description: 'wash the dishes',
-  },
-  {
-    index: 3,
-    completed: false,
-    description: 'wash the dishes',
-  },
+let content = JSON.parse(localStorage.getItem('taskContent')) || [{
+  index: 0,
+  completed: true,
+  description: 'wash the dishes'
+},
+{
+  index: 1,
+  completed: false,
+  description: 'wash the dishes',
+},
+{
+  index: 2,
+  completed: true,
+  description: 'wash the dishes',
+},
+{
+  index: 3,
+  completed: false,
+  description: 'wash the dishes',
+},
+{
+  index: 4,
+  completed: false,
+  description: 'wash the dishes',
+},
 ];
 
 function displayTask() {
-  if (content !== []) {
+  if (content != []) {
     content.forEach((element) => {
       const structure = ` <li class='List-items' id='${element.index}'>
       <input type='checkbox' class='box' data-id='${element.index}'  ${element.completed ? 'checked' : ''}>
       <input type='text' value='${element.description}' data-index='${element.index}' class='description ${element.completed ? 'completed' : ''}'>
       <i class='fas fa-ellipsis-v move-element' data-id='${element.index}'></i>
-      </li>
-      <hr>
-      `;
+      </li> `
       container.innerHTML += structure;
     });
   }
+  const checkbox = document.querySelectorAll('.box');
+  checkbox.forEach((chbox) => {
+    chbox.addEventListener('change', updateStatus);
+  });
 }
 
+window.addEventListener('DOMContentLoaded', () => {
+  if (!localStorage.getItem('taskContent')) {
+    localStorage.setItem('taskContent', JSON.stringify(content))
+  }
+});
 displayTask();
