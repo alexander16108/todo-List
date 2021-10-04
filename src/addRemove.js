6563a21b8c02a9b72b8eccb1dfb5ee079cb35c67
alexander16108 <alexandermayowa05@gmail.com>
2021-10-04 15:35:21 +0100

    removing drag and drop from project

--- src/addRemove.js
+++ src/addRemove.js
@@ -1,47 +1,43 @@
 /* eslint-disable no-plusplus */
 import './style.css';
-import dragDrop from './dragdrop.js';
 import statusUpdate from './server.js';
 
-class Task {
-  constructor() {
-    this.size = 0;
-    this.ul = document.getElementById('task-list');
-    this.savedTasks = JSON.parse(localStorage.getItem('savedTasks')) || [];
+    let size = 0;
+    const ul = document.getElementById('task-list');
+    let savedTasks = JSON.parse(localStorage.getItem('savedTasks')) || [];
+
+  const clearContent = () => {
+    savedTasks = savedTasks.filter((object) => object.completed !== true);
+    localStorage.setItem('savedTasks', JSON.stringify(savedTasks));
+    displayAllTask();
   }
 
-  clearContent() {
-    this.savedTasks = this.savedTasks.filter((object) => object.completed !== true);
-    localStorage.setItem('savedTasks', JSON.stringify(this.savedTasks));
-    this.displayAllTask();
-  }
-
-  addIndex() {
-    if (this.savedTasks.length < 1) {
-      return this.size;
+  const addIndex = () => {
+    if (savedTasks.length < 1) {
+      return size;
     }
-    return this.savedTasks[this.savedTasks.length - 1].index + 1;
+    return savedTasks[savedTasks.length - 1].index + 1;
   }
 
-  addTask(book) {
-    this.savedTasks.push(book);
-    localStorage.setItem('savedTasks', JSON.stringify(this.savedTasks));
-    this.size += 1;
+  const addTask = (book) => {
+    savedTasks.push(book);
+    localStorage.setItem('savedTasks', JSON.stringify(savedTasks));
+    size += 1;
   }
 
-  updateList() {
+  const updateList = () => {
     let newIndex = 0;
-    this.savedTasks = JSON.parse(localStorage.getItem('savedTasks')) || [];
+    savedTasks = JSON.parse(localStorage.getItem('savedTasks')) || [];
     // eslint-disable-next-line no-return-assign
-    this.savedTasks.filter((obj) => (obj.index = newIndex++));
-    localStorage.setItem('savedTasks', JSON.stringify(this.savedTasks));
+    savedTasks.filter((obj) => (obj.index = newIndex++));
+    localStorage.setItem('savedTasks', JSON.stringify(savedTasks));
   }
 
-  editTask(e, i) {
+  const editTask = (e, i) => {
     if (e.key === 'Enter') {
-      this.savedTasks[i].description = e.target.innerHTML;
-      localStorage.setItem('savedTasks', JSON.stringify(this.savedTasks));
-      this.displayAllTask();
+      savedTasks[i].description = e.target.innerHTML;
+      localStorage.setItem('savedTasks', JSON.stringify(savedTasks));
+      displayAllTask();
       e.preventDefault();
     }
     e.target.parentNode.children[1].contentEditable = true;
@@ -50,31 +46,23 @@ class Task {
   // to Delete task from the list and
   // updating the local storage at the same time
 
-  deleteTask(e, i) {
-    this.savedTasks.splice(i, 1);
-    localStorage.setItem('savedTasks', JSON.stringify(this.savedTasks));
-    this.displayAllTask();
+  const deleteTask = (e, i) => {
+    savedTasks.splice(i, 1);
+    localStorage.setItem('savedTasks', JSON.stringify(savedTasks));
+    displayAllTask();
   }
 
   // Creating Elements Individually and
   //  displaying the on the todo List
 
-  displayAllTask() {
-    this.ul.innerHTML = '';
-    this.updateList();
+ const displayAllTask = () => {
+    ul.innerHTML = '';
+    updateList();
 
-    this.savedTasks.forEach((task, i) => {
+    savedTasks.forEach((task, i) => {
       const li = document.createElement('li');
       li.className = 'List-items';
-      li.draggable = 'true';
-      li.addEventListener('dragstart', (e) => {
-        dragDrop(e, i);
-      });
-
-      li.addEventListener('dragend', (e) => {
-        dragDrop(e, i);
-        this.displayAllTask();
-      });
+      li.draggable = 'false';
 
       const p = document.createElement('p');
       p.className = 'description';
@@ -85,7 +73,7 @@ class Task {
       checkboxes.checked = task.completed;
       checkboxes.addEventListener('change', (e) => {
         statusUpdate(e, i);
-        this.displayAllTask();
+        displayAllTask();
       });
 
       const index = document.createElement('span');
@@ -93,7 +81,7 @@ class Task {
       const button = document.createElement('i');
       button.classList.add('fas', 'fa-ellipsis-v', 'move-item');
       p.addEventListener('keydown', (e) => {
-        this.editTask(e, i);
+        editTask(e, i);
       });
       button.addEventListener('click', (e) => {
         button.classList.add('fa-trash-alt', 'trash');
@@ -101,41 +89,35 @@ class Task {
         document
           .querySelector('.fa-trash-alt')
           .addEventListener('click', (e) => {
-            this.deleteTask(e, i);
+            deleteTask(e, i);
           });
-        this.editTask(e, i);
+        editTask(e, i);
       });
 
       p.innerHTML = task.description;
-      // index.innerHTML = task.index;
 
       li.appendChild(checkboxes);
       li.appendChild(p);
       li.appendChild(index);
       li.appendChild(button);
-      this.ul.appendChild(li);
-      this.ul.addEventListener('dragover', (e) => {
-        dragDrop(e, i);
-      });
+      ul.appendChild(li);
     });
   }
-}
 
-const newTask = new Task();
 
-export default function addTask(v) {
-  newTask.addTask({
+export default function addTasks(v) {
+  addTask({
     description: v,
     completed: false,
-    index: newTask.addIndex(),
+    index: addIndex(),
   });
-  newTask.displayAllTask();
+  displayAllTask();
 }
 
 document.getElementById('clearAll').addEventListener('click', () => {
-  newTask.clearContent();
+  clearContent();
 });
 
 window.addEventListener('load', () => {
-  newTask.displayAllTask();
+  displayAllTask();
 });
--- src/check.js
+++
@@ -1,6 +0,0 @@
-export default function updateStatus(event) {
-  event.target.nextElementSibling.classList.toggle('completed');
-  const status = JSON.parse(localStorage.getItem('toDo'));
-  status[event.target.dataset.id].completed = event.target.checked;
-  localStorage.setItem('toDo', JSON.stringify(status));
-}
