5ea98f405811b1b676563e7690be5bb1afc7d788
alexander16108 <alexandermayowa05@gmail.com>
2021-10-08 15:31:43 +0100

    Mocking completed status

---
+++ src/__mock__/status.js
@@ -0,0 +1,10 @@
+import localStorageMock from './localStorage.js';
+
+const statusUpdate = (e, i) => {
+  const savedTasks = localStorageMock.getItem('savedTasks');
+  savedTasks[i].completed = !savedTasks[i].completed;
+  e.target.checked = savedTasks[i].completed;
+  localStorageMock.setItem('savedTasks', savedTasks);
+}
+
+module.exports = statusUpdate;
