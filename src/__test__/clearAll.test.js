c90ed67960cb5b7bb31f10549c669854161c96d7
alexander16108 <alexandermayowa05@gmail.com>
2021-10-08 20:00:26 +0100

    fixing errors

--- src/__test__/clearAll.test.js
+++ src/__test__/clearAll.test.js
@@ -2,26 +2,26 @@
  * @jest-environment jsdom
  */
 
- import localStorageMock from '../__mock__/localStorage.js';
- import clearContent from '../__mock__/clearAll.js';
- 
- describe('Remove completed tasks from list', () => {
+import localStorageMock from '../__mock__/localStorage.js';
+import clearContent from '../__mock__/clearAll.js';
+
+describe('Remove completed tasks from list', () => {
   const savedTasks = [
     {
-       description: 'make noodles',
-       completed: false,
-       index: 1,
+      description: 'make noodles',
+      completed: false,
+      index: 1,
     },
     {
-       description: 'eat noodles',
-       completed: true,
-       index: 2,
+      description: 'eat noodles',
+      completed: true,
+      index: 2,
     },
   ];
-   test('should return an array without completed tasks', () => {
-     expect(clearContent(savedTasks)).toHaveLength(1);
-   });
-   test('should be updated after deleting the tasks', () => {
-     expect(localStorageMock.getItem('savedTasks')).toStrictEqual(clearContent(savedTasks));
-   });
- });
+  test('should return an array without completed tasks', () => {
+    expect(clearContent(savedTasks)).toHaveLength(1);
+  });
+  test('should be updated after deleting the tasks', () => {
+    expect(localStorageMock.getItem('savedTasks')).toStrictEqual(clearContent(savedTasks));
+  });
+});
