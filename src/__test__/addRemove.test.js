feb7ea12e920f7a3f7d67eaa85f198610b58ad61
alexander16108 <alexandermayowa05@gmail.com>
2021-10-08 09:08:58 +0100

    pushing files to github

--- src/__test__/addRemove.test.js
+++ src/__test__/addRemove.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import LocalStorage from '../server.js';
+import updateStatus from '../server.js';
 
 describe('test add elements', () => {
   document.body.innerHTML = '<div>'
@@ -12,66 +12,70 @@ describe('test add elements', () => {
   + '</ul>'
   + '</div>';
 
-  const event = new KeyboardEvent('keydown', { key: 'Enter' });
-
-  const localStorage = new LocalStorage();
-
   const tasksList = document.querySelector('.task-list');
 
   let toDoList = [];
 
-  const addTodoToList = (task) => {
-    const taskItem = document.createElement('li');
-    const checkBox = document.createElement('input');
-    const taskDescription = document.createElement('label');
-    const checkDescription = document.createElement('div');
-    const icon = document.createElement('i');
-    icon.classList.add('fas', 'fa-ellipsis-v', 'list-icon');
-    taskItem.classList.add('element-border');
-    taskItem.setAttribute('data-id', task.index);
-    taskItem.setAttribute('id', `Task${task.index}`);
-    checkBox.classList.add('list-checkbox');
-    checkBox.setAttribute('type', 'checkbox');
-    checkBox.checked = task.completed;
-    taskDescription.innerHTML = task.description;
-    if (task.completed) {
-      taskDescription.style.textDecoration = 'line-through';
-    }
-    checkDescription.classList.add('check-description');
-    checkDescription.appendChild(checkBox);
-    checkDescription.appendChild(taskDescription);
-    taskItem.appendChild(checkDescription);
-    taskItem.appendChild(icon);
-    tasksList.appendChild(taskItem);
-  };
+  const displayAllTask = () => {
+    ul.innerHTML = '';
+    // updateList();
 
-  const displayTasks = () => {
-    tasksList.innerHTML = '';
-    toDoList.forEach((task) => addTodoToList(task));
-  };
+    savedTasks.forEach((task, i) => {
+      const li = document.createElement('li');
+      li.classList.add('List-items', 'flex-btw');
+      li.draggable = 'false';
 
-  const checkDom = () => [...tasksList.children];
+      const p = document.createElement('p');
+      p.classList.add( 'description', 'bd-ol');
+
+      const checkboxes = document.createElement('input');
+      checkboxes.type = 'checkbox';
+      checkboxes.className = 'box';
+      checkboxes.checked = task.completed;
+      if (task.completed === true) {
+        checkboxes.checked = p.classList.add('completed');
+      }else {
+        checkboxes.checked = p.classList.remove('completed');
+      }
+      checkboxes.addEventListener('change', (e) => {
+        statusUpdate(e, i);
+        displayAllTask();
+      });
+
+      const index = document.createElement('span');
+      index.className = 'index';
+      const button = document.createElement('i');
+      button.classList.add('fas', 'fa-ellipsis-v', 'move-item', 'flex-md', 'bd-ol');
+      p.addEventListener('keydown', (e) => {
+        editTask(e, i);
+      });
+      button.addEventListener('click', (e) => {
+        button.classList.add('fa-trash-alt', 'trash', 'flex-md', 'bd-ol');
+        document.querySelector('.fa-trash-alt').addEventListener('click', (e) => {deleteTask(e, i)})
+        editTask(e, i);
+      });
+
+      p.innerHTML = task.description;
+
+      li.appendChild(checkboxes);
+      li.appendChild(p);
+      li.appendChild(index);
+      li.appendChild(button);
+      ul.appendChild(li);
+    });
+  }
 
   // Act
-  const add = (event) => {
-    if (event.key === 'Enter') {
-      toDoList = localStorage.getDataLocalStorage();
-      const input = document.querySelector('.input-task');
-      const inputValue = input.value;
-      const task = {
-        description: inputValue,
-        completed: false,
-        index: toDoList.length + 1,
-      };
-      toDoList.push(task);
-      localStorage.setDataLocalStorage(toDoList);
-      displayTasks();
-      return toDoList;
-    }
-    return false;
-  };
-
-  const addTodo = add(event);
+  const addTask = (book) => {
+    addTask({
+      description: book,
+      completed: false,
+      index: addIndex(),
+    });
+    savedTasks.push(book);
+    localStorage.setItem('savedTasks', JSON.stringify(savedTasks));
+    size += 1;
+  }
 
   // Assert
   test('add task to the list', () => {
@@ -93,88 +97,88 @@ describe('test add elements', () => {
   });
 });
 
-describe('remove method', () => {
-  // Arrange
-  document.body.innerHTML = '<ul class="tasks-list">'
-  + '<li data-id = "1"><input type="checkbox" checked><p>test 1</p></li>'
-  + '<li data-id = "2"><input type="checkbox"><p>test 1</p></li>'
-  + '<li data-id = "3"><input type="checkbox" checked><p>test 3</p></li>'
-  + '</ul>';
+// describe('remove method', () => {
+//   // Arrange
+//   document.body.innerHTML = '<ul class="task-list">'
+//   + '<li data-id = "1"><input type="checkbox" checked><p>test 1</p></li>'
+//   + '<li data-id = "2"><input type="checkbox"><p>test 1</p></li>'
+//   + '<li data-id = "3"><input type="checkbox" checked><p>test 3</p></li>'
+//   + '</ul>';
 
-  const tasksList = document.querySelector('.tasks-list');
+//   const tasksList = document.querySelector('.tasks-list');
 
-  let toDoList = [
-    {
-      description: 'test 1',
-      completed: false,
-      index: 1,
-    },
-    {
-      description: 'test 2',
-      completed: false,
-      index: 2,
-    },
-    {
-      description: 'test 3',
-      completed: false,
-      index: 3,
-    },
-  ];
+//   let toDoList = [
+//     {
+//       description: 'test 1',
+//       completed: false,
+//       index: 1,
+//     },
+//     {
+//       description: 'test 2',
+//       completed: false,
+//       index: 2,
+//     },
+//     {
+//       description: 'test 3',
+//       completed: false,
+//       index: 3,
+//     },
+//   ];
 
-  const localStorage = new LocalStorage();
-  localStorage.setDataLocalStorage(toDoList);
+//   const localStorage = new LocalStorage();
+//   localStorage.setDataLocalStorage(toDoList);
 
-  const updateArray = () => {
-    const items = [...tasksList.children];
+//   const updateArray = () => {
+//     const items = [...tasksList.children];
 
-    toDoList = [];
+//     toDoList = [];
 
-    items.forEach((item, index) => {
-      const newTask = {
-        description: item.lastChild.textContent,
-        completed: item.firstChild.checked,
-        index: index + 1,
-      };
-      toDoList.push(newTask);
-    });
-    localStorage.setDataLocalStorage(toDoList);
-  };
+//     items.forEach((item, index) => {
+//       const newTask = {
+//         description: item.lastChild.textContent,
+//         completed: item.firstChild.checked,
+//         index: index + 1,
+//       };
+//       toDoList.push(newTask);
+//     });
+//     localStorage.setDataLocalStorage(toDoList);
+//   };
 
-  const taskElem = document.querySelector('li[data-id="3"]');
+//   const taskElem = document.querySelector('li[data-id="3"]');
 
-  // Act
-  const removeElement = (element) => {
-    if (element) {
-      tasksList.removeChild(element);
-      updateArray();
-      return toDoList;
-    }
-    return false;
-  };
+//   // Act
+//   const removeElement = (element) => {
+//     if (element) {
+//       tasksList.removeChild(element);
+//       updateArray();
+//       return toDoList;
+//     }
+//     return false;
+//   };
 
-  // Assert
-  test('remove an element from the array', () => {
-    expect(removeElement(taskElem)).toHaveLength(2);
-  });
+//   // Assert
+//   test('remove an element from the array', () => {
+//     expect(removeElement(taskElem)).toHaveLength(2);
+//   });
 
-  test('remove element from the list', () => {
-    expect([...tasksList.children].length).toBe(2);
-  });
-});
+//   test('remove element from the list', () => {
+//     expect([...tasksList.children].length).toBe(2);
+//   });
+// });
 
-describe('local storage management', () => {
-  const localStorage = new LocalStorage();
-  const arr = [{
-    description: 'local storage test',
-    completed: false,
-    index: 1,
-  }];
-  localStorage.setDataLocalStorage(arr);
-  test('it stores items', () => {
-    expect(localStorage.getDataLocalStorage()).toEqual([{
-      description: 'local storage test',
-      completed: false,
-      index: 1,
-    }]);
-  });
-});
+// describe('local storage management', () => {
+//   const localStorage = new LocalStorage();
+//   const arr = [{
+//     description: 'local storage test',
+//     completed: false,
+//     index: 1,
+//   }];
+//   localStorage.setDataLocalStorage(arr);
+//   test('it stores items', () => {
+//     expect(localStorage.getDataLocalStorage()).toEqual([{
+//       description: 'local storage test',
+//       completed: false,
+//       index: 1,
+//     }]);
+//   });
+// });
